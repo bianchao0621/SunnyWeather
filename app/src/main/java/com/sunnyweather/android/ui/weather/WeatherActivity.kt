e0533a33db807c02e5d07827277dea4b6f31a6a3
bianchao <bianchao0621@163.com>
2021-10-02 17:36:20 +0800

    修改2

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -106,12 +106,12 @@ class WeatherActivity : AppCompatActivity() {
             val temperature = daily.temperature[i]
             val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
             forecastLayout, false)
-            val dateInfo = view.findViewById(R.id.dataInfo) as TextView
+            val dateInfo = view.findViewById(R.id.dateInfo) as TextView
             val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
             val skyInfo = view.findViewById(R.id.skyInfo) as TextView
             val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
-            val simpleDataFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            dataInfo.text = simpleDataFormat.format(skycon.date)
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dateInfo.text = simpleDateFormat.format(skycon.date)
             val sky = getSky(skycon.value)
             skyIcon.setImageResource(sky.icon)
             skyInfo.text = sky.info
